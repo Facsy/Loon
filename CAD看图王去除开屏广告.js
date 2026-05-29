75b5d4cf41aca1b6de443fa2aa71b1aa2bfbaa46
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-29 20:01:33 +0800

    Update CAD看图王去除开屏广告.js

--- CAD看图王去除开屏广告.js
+++ CAD看图王去除开屏广告.js
@@ -1,11 +1,8 @@
 #!name = CAD看图王去除开屏广告
 #!desc = CAD看图王去除开屏广告
-#!Home page = 6
-#!Author = 7
+Home page = 6
+Author = 7
 #!icon = https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/CAD看图王.png
   
 [Rewrite]
 ^https?:\/\/119\.29\.29\.\d+\/d reject-200
-
-[MITM]
-hostname = 
