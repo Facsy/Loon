4f33d62ed670ca6a4a3ba0e644a1aa37bcdd83a4
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-29 18:41:21 +0800

    Update Forward_vip.js

--- Forward_vip.js
+++ Forward_vip.js
@@ -1,6 +1,7 @@
 #!name = Forward_vip
 #!desc = 最高支持1.3.13
-
+#!icon= https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Advertising.png
+  
 [Rewrite]
 ^https:\/\/fluxapi\.vvebo\.vip\/v1\/(purchase\/iap\/subscription) header https://mock.forward1.workers.dev/forward/v1/$1
 
