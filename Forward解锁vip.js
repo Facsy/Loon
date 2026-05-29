fa193a31ee8772f27b5cc4e43ddffbd7e247647e
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-29 20:17:58 +0800

    Update Forward解锁vip.js

--- Forward解锁vip.js
+++ Forward解锁vip.js
@@ -1,9 +1,10 @@
-#!name = Forward解锁vip
-#!desc = 最高支持1.3.13
-#!icon = https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/Forward.png
+#!name=Forward解锁vip
+#!desc=最高支持1.3.13
+#!icon=https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/Forward.png#!homepage=https://github.com/Facsy/Loon
+#!author=Facsy
   
 [Rewrite]
 ^https:\/\/fluxapi\.vvebo\.vip\/v1\/(purchase\/iap\/subscription) header https://mock.forward1.workers.dev/forward/v1/$1
 
 [MITM]
-hostname = fluxapi.vvebo.vip
+hostname=fluxapi.vvebo.vip
