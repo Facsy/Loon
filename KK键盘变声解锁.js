0ac9dc76b51cba57ca0f9bd1b5b55ed25e190e76
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-29 20:15:11 +0800

    Update KK键盘变声解锁.js

--- KK键盘变声解锁.js
+++ KK键盘变声解锁.js
@@ -1,10 +1,10 @@
-#!name = KK键盘变声解锁
-#!desc = 解锁无限变声功能
-#!icon = https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/KK键盘.png
+#!name=KK键盘变声解锁
+#!desc=解锁无限变声功能
+#!icon=https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/KK键盘.png
 
 [Rewrite]
 ^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/(checkCount|consumeCount) response-body-json-jq '.data.totalCount = 999 | .data.currCount = 999'
 ^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/createTtsAudio response-body-json-jq '.data.freeCount = 999'
 
 [MITM]
-hostname = kk.weshine.im
+hostname=kk.weshine.im
