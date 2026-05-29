48197c6027a75dd7ac3c1eeeb6521e6e25a4fa61
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-29 20:16:52 +0800

    Update KK键盘变声解锁.js

--- KK键盘变声解锁.js
+++ KK键盘变声解锁.js
@@ -1,7 +1,9 @@
 #!name=KK键盘变声解锁
 #!desc=解锁无限变声功能
 #!icon=https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/KK键盘.png
-
+#!homepage=https://github.com/Facsy/Loon
+#!author=Facsy
+  
 [Rewrite]
 ^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/(checkCount|consumeCount) response-body-json-jq '.data.totalCount = 999 | .data.currCount = 999'
 ^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/createTtsAudio response-body-json-jq '.data.freeCount = 999'
