2590767338b252b8285ac8167f592d2746928f7e
Facsy <288818614+Facsy@users.noreply.github.com>
2026-05-30 11:25:39 +0800

    Update and rename CAD看图王去除开屏广告.js to CAD看图王屏蔽开屏广告.js

--- CAD看图王屏蔽开屏广告.js
+++ CAD看图王屏蔽开屏广告.js
@@ -1,5 +1,6 @@
-#!name=CAD看图王去除开屏广告
-#!desc=CAD看图王去除开屏广告
+#!name=CAD看图王屏蔽开屏广告
+#!desc=CAD看图王屏蔽开屏广告
+#!openUrl=https://apps.apple.com/app/id456933691
 #!icon=https://raw.githubusercontent.com/Facsy/Loon/refs/heads/main/icon/CAD看图王.png
 #!homepage=https://github.com/Facsy/Loon
 #!author=Facsy
